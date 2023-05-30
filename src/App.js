b65181e632779793acd55572aeeaf63b14172fa8
gaurangpatil239 <gaurantgpatil2309@gmail.com>
2023-05-31 00:51:22 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
-import { BrowserRouter } from 'react-router-dom';
 
 
 function App() {
